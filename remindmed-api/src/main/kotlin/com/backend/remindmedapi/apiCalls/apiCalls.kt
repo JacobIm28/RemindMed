9e50f69ab633e41e79291cedb64809d3b7c22276
Samir Ali <s295ali@uwaterloo.ca>
2024-03-08 13:02:39 -0500

    added medicine controller and fixed apiCalls

--- remindmed-api/src/main/kotlin/com/backend/remindmedapi/apiCalls/apiCalls.kt
+++ remindmed-api/src/main/kotlin/com/backend/remindmedapi/apiCalls/apiCalls.kt
@@ -1,6 +1,8 @@
-package com.gradle.apiCalls
+package com.backend.remindmedapi.apiCalls
 
 import com.google.gson.Gson
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
 import io.ktor.client.*
 import io.ktor.client.engine.android.*
 import io.ktor.client.request.*
@@ -8,37 +10,43 @@ import io.ktor.client.statement.*
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.*
 
 
 // loads in information related to a single drug
 @OptIn(DelicateCoroutinesApi::class)
-fun callOpenFdaSingleDrug(drug: String) {
+fun callOpenFdaSingleDrug(drug: String): JsonObject {
     val client = HttpClient(Android)
     try {
+        var data: JsonObject = JsonObject()
+        var res: String? = null
         runBlocking{
             launch{
-                val response: String = client.get("https://api.fda.gov/drug/label.json?search=openfda.brand_name:$drug&limit=1").bodyAsText()
-                val data: SingleDrugRes = Gson().fromJson(response, SingleDrugRes::class.java)
-                println(data)
+                val response: String = client.get("https://api.fda.gov/drug/label.json?search=$drug&limit=1").bodyAsText()
+                // cast the response to a JsonObject of type SingleDrugRes
+                data = JsonParser.parseString(response).asJsonObject
+
             }
         }
+        return data
     } catch(exception: Exception) {
         println(exception);
+        return JsonObject()
     }
 }
 
 // for our search bar later on. Ques up a whole list of drugs based on string input
 // will use the rxnorm approximate match API
-fun callListOfMedication(strSoFar: String) {
+fun callListOfMedication(strSoFar: String): MutableList<String> {
     val client = HttpClient(Android)
     try {
+        val res = mutableListOf<String>()
         runBlocking{
             launch{
-                val response: String = client.get("https://rxnav.nlm.nih.gov/REST/approximateTerm.json?term=$strSoFar&maxEntries=100").bodyAsText()
-                val data: rxNormData= Gson().fromJson(response, rxNormData::class.java)
+                val response: String = client.get("https://rxnav.nlm.nih.gov/REST/approximateTerm.json?term=$strSoFar&maxEntries=10000").bodyAsText()
+                val data: RxNormData= Gson().fromJson(response, RxNormData::class.java)
 
                 // will now begin looping through the candidates field and adding valid elements to our return array
-                val res: MutableList<String> = mutableListOf("");
                 val numReturn : Int = 10;
                 var numSoFar : Int = 0
                 for (candidate in data.approximateGroup.candidate) {
@@ -51,10 +59,10 @@ fun callListOfMedication(strSoFar: String) {
                     }
                 }
                 res.removeFirst()
-                println(res)
             }
         }
+        return res
     } catch(exception: Exception) {
-        println(exception);
+        return mutableListOf()
     }
 }
