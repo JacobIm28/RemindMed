084421667573ec29b145c05a51e9513af6b8d7f2
Gen Nishiwaki <gnishiwaki@uwaterloo.ca>
2024-04-07 17:49:17 -0400

    Making more test cases

--- RemindMed/app/src/test/java/com/example/remindmed/DoctorProfileUnitTests.kt
+++ RemindMed/app/src/test/java/com/example/remindmed/DoctorProfileUnitTests.kt
@@ -7,39 +7,67 @@ import com.gradle.ui.viewModels.LoginViewModel
 import com.gradle.ui.views.shared.ProfileViewEvent
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import android.text.TextUtils
+import android.util.Base64
 
 
 internal class DoctorProfileUnitTests {
-    private val testDoc: Doctor = Doctor("1", "Gen", "gen@gmail.com")
 
     @Test
     fun viewModelTests() {
-        // changing the various fields
-        var viewModDoc: DoctorViewModel = DoctorViewModel(testDoc)
-
+        val testDoc: Doctor = Doctor("1", "Gen", "gen@gmail.com")
+        val viewModDoc: DoctorViewModel = DoctorViewModel(testDoc)
         assertEquals(viewModDoc.name.value, testDoc.name)
         assertEquals(viewModDoc.email.value, testDoc.email)
         assertEquals(viewModDoc.successfulChange.value, testDoc.successfulChange)
         assertEquals(viewModDoc.submitEnabled.value, testDoc.submitEnabled)
 
-        testDoc.name = "Ben"
-        assertEquals("Ben", viewModDoc.name.value)
-        testDoc.email = "ben@gmail.com"
-        assertEquals("ben@gmail.com", viewModDoc.email.value)
+        val testDoc2: Doctor = Doctor("1", "Gen", "gen@gmail.com")
+        val viewModDoc2: DoctorViewModel = DoctorViewModel(testDoc2)
+        testDoc2.name = "Ben"
+        assertEquals("Ben", viewModDoc2.name.value)
+
+        val testDoc3: Doctor = Doctor("1", "Gen", "gen@gmail.com")
+        val viewModDoc3: DoctorViewModel = DoctorViewModel(testDoc3)
+        testDoc3.email = "ben@gmail.com"
+        assertEquals("ben@gmail.com", viewModDoc3.email.value)
     }
 
     @Test
     fun controllerTests() {
         val loginViewModel = LoginViewModel()
-        val controller = DoctorController(testDoc, loginViewModel)
 
+        val testDoc1: Doctor = Doctor("1", "Gen", "gen@gmail.com")
+        val controller = DoctorController(testDoc1, loginViewModel)
         controller.invoke(ProfileViewEvent.NameEvent, "Jacob")
-        assertEquals("Jacob", testDoc.name)
+        assertEquals("Jacob", testDoc1.name)
 
-        controller.invoke(ProfileViewEvent.EmailEvent, "not an email")
-        assertEquals(false, testDoc.submitEnabled)
+        val testDoc2: Doctor = Doctor("1", "Gen", "gen@gmail.com")
+        val controller2 = DoctorController(testDoc2, loginViewModel)
+        controller2.invoke(ProfileViewEvent.EmailEvent, "not an email")
+        assertEquals(false, testDoc2.submitEnabled)
 
-        controller.invoke(ProfileViewEvent.DismissEvent, "")
-        assertEquals(false, testDoc.changesSubmitted)
+        val testDoc3: Doctor = Doctor("1", "Gen", "gen@gmail.com")
+        val controller3 = DoctorController(testDoc3, loginViewModel)
+        controller3.invoke(ProfileViewEvent.DismissEvent, "")
+        assertEquals(false, testDoc3.changesSubmitted)
+    }
+
+    @Test
+    fun allAspects() {
+        val loginViewModel = LoginViewModel()
+
+        val testDoc1: Doctor = Doctor("1", "Gen", "gen@gmail.com")
+        val controller1 = DoctorController(testDoc1, loginViewModel)
+        val viewModDoc1: DoctorViewModel = DoctorViewModel(testDoc1)
+        viewModDoc1.email.value = "gen2@gmail.com"
+        controller1.invoke(ProfileViewEvent.UpdateEvent, "")
+        assertEquals(true, testDoc1.changesSubmitted)
+
+        val testDoc2: Doctor = Doctor("1", "Gen", "gen@gmail.com")
+        val controller2 = DoctorController(testDoc2, loginViewModel)
+        val viewModDoc2: DoctorViewModel = DoctorViewModel(testDoc2)
+        controller2.invoke(ProfileViewEvent.LogoutClicked, "")
+        assertEquals(true, viewModDoc2.logoutClicked.value)
     }
 }
